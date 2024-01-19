b50198e558064ba839c5ce3b3d050edb69d0a625
bomanaps <mercy.boma35@yahoo.com>
2024-01-19 08:40:10 -0800

    fix

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# code to create file with permissions
+node default {
+file {'/tmp/school':
+ensure  => 'present',
+content => 'I love Puppet',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data'
+}
+}
